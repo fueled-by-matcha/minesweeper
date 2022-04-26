8a7550031dd2348676d00dea5095590ff4b0f51e
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-04-25 22:51:07 -0400

    finished setup
    -created 3 main frames and finished debugging
    -finished utilities to adjust height and width

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from tkinter import *
 import settings
+import utilities
 
 root = Tk()
 # Override the settings of the window
 root.configure(bg = "black")
-root.geometry(f'{settings.WIDTH} x {settings.HEIGHT}')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper Game")
 root.resizable(False, False)
 
 # Create top frame
 top_frame = Frame(
     root,
-    bg = "red", #change to black
-    width = 1440,
-    height = 180
+    bg = "black", #change to black
+    width = settings.WIDTH,
+    height = utilities.height_prct(25)
 )
 top_frame.place(x = 0, y = 0)
 
 # Create left frame
 left_frame = Frame(
     root,
-    bg = "blue",
-    width = 360,
-    height = 540
+    bg = "black",
+    width = utilities.width_prct(25),
+    height = utilities.height_prct(75)
 )
-left_frame.place(x = 0, y = 180)
+left_frame.place(x = 0, y = utilities.height_prct(25))
 
+# Create center frame
+center_frame = Frame(
+    root,
+    bg = "black",
+    width = utilities.width_prct(75),
+    height = utilities.height_prct(75)
+)
+center_frame.place(x = utilities.width_prct(25), y = utilities.height_prct(25))
 #Run the window
 root.mainloop()
