d5c0f744d7bab846b86222ffb8007d1348eb0e24
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-04-25 22:41:09 -0400

    removing hard coding
    created settings file for constants
    created utilities to mess with settings

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from tkinter import *
+import settings
 
 root = Tk()
-# override the settings of the window
+# Override the settings of the window
 root.configure(bg = "black")
-root.geometry('1440x720')
+root.geometry(f'{settings.WIDTH} x {settings.HEIGHT}')
 root.title("Minesweeper Game")
 root.resizable(False, False)
 
-#Create top frame
+# Create top frame
 top_frame = Frame(
     root,
     bg = "red", #change to black
@@ -16,7 +17,7 @@ top_frame = Frame(
 )
 top_frame.place(x = 0, y = 0)
 
-#Create left frame
+# Create left frame
 left_frame = Frame(
     root,
     bg = "blue",
@@ -24,5 +25,6 @@ left_frame = Frame(
     height = 540
 )
 left_frame.place(x = 0, y = 180)
+
 #Run the window
 root.mainloop()
