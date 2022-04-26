997ff791a877c30fbd5aced045e5098145128ad6
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-04-26 00:32:35 -0400

    logic to display mines
    logic that updates buttons with amount of mines surrounding the cell

--- cell.py
+++ cell.py
@@ -16,16 +16,52 @@ class Cell:
         btn = Button(
             location,
             width = 12,
-            height = 4,
-            text = f"{self.x},{self.y}"
+            height = 4
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
     
     def left_click_actions(self, event):
-        print(event)
-        print("I am left clicked!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+    
+    def get_cell_by_axis(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property            
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self. y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+        return counter
+
+    def show_cell(self):
+        self.cell_btn_object.configure(text = self.surrounded_cells_mines_length)
+
+    def show_mine(self):
+        # Logic to interrupt game and display message
+        self.cell_btn_object.configure(bg = "red")
 
     def right_click_actions(self, event):
         print(event)
